e61baf000cfc25a527e8a589ac10d1e1a1aafec5
Jacqueline Kong <jek2179@columbia.edu>
2023-02-10 11:09:05 -0800

    Add a player scoreboard and buttons that increment each player's score
    Throughline: Get something hooked up that will save the score

--- src/Game.test.tsx
+++ src/Game.test.tsx
@@ -1,9 +1,29 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import Game from "./Game";
+import Game, { GameState, PlayerScoreboard } from "./Game";
 
 test("renders title", () => {
   render(<Game />);
-  const linkElement = screen.getByText(/Rock Paper Scissors!/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = screen.getByText(/Rock Paper Scissors!/i);
+  expect(titleElement).toBeInTheDocument();
+});
+
+test("renders scoreboard", () => {
+  render(<Game />);
+  const p1Score = screen.getByText(/Player 1 Score/i);
+  const p2Score = screen.getByText(/Player 2 Score/i);
+  expect(p1Score).toBeInTheDocument();
+  expect(p2Score).toBeInTheDocument();
+});
+
+test("PlayerScoreboard renders scores", () => {
+  const testProps: GameState = {
+    player1Score: 4,
+    player2Score: 0,
+  };
+  render(<PlayerScoreboard {...testProps} />);
+  const p1Score = screen.getByText(/Player 1 Score: 4/i);
+  const p2Score = screen.getByText(/Player 2 Score: 0/i);
+  expect(p1Score).toBeInTheDocument();
+  expect(p2Score).toBeInTheDocument();
 });
--- src/Game.tsx
+++ src/Game.tsx
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, { useState } from "react";
 
 function Game() {
+  const [player1Score, setplayer1Score] = useState(0);
+  const [player2Score, setplayer2Score] = useState(0);
+
   return (
     <div>
       <h1>Rock Paper Scissors!</h1>
+      <PlayerScoreboard
+        player1Score={player1Score}
+        player2Score={player2Score}
+      />
+      <PlayerControls
+        incrementScore={() => setplayer1Score(player1Score + 1)}
+      />
+      <PlayerControls
+        incrementScore={() => setplayer2Score(player2Score + 1)}
+      />
     </div>
   );
 }
+
+export interface GameState {
+  player1Score: number;
+  player2Score: number;
+}
+
+export const PlayerScoreboard = (props: GameState): JSX.Element => {
+  return (
+    <div>
+      <p>Player 1 Score: {props.player1Score}</p>
+      <p>Player 2 Score: {props.player2Score}</p>
+    </div>
+  );
+};
+
+export const PlayerControls = (props: {
+  incrementScore: () => void;
+}): JSX.Element => {
+  return (
+    <div>
+      <button onClick={props.incrementScore}>Increase Score</button>
+    </div>
+  );
+};
 
 export default Game;
